ebdbcdb94ebc836ff942b89b553e07a0b9ed0866
Volodymyr Basok <volodymyrquo@gmail.com>
2021-08-23 11:31:30 +0300

    created basics file

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MainContent = () => {
+  return <div>Hello from Main content</div>
+}
+
+export default MainContent
---
+++ src/components/NavigationSidebar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NavigationSidebar = () => {
+  return <div>Hello from NavigationSidebar</div>
+}
+
+export default NavigationSidebar
